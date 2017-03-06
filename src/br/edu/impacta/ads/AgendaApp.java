21d6172348f504b73119ae089aec4b48669c957e
1600043 <1600043@Sala31225.fit.net>
2017-03-06 09:32:13 -0300

    ainda não terminado

---
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -0,0 +1,17 @@
+package br.edu.impacta.ads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class AgendaApp {
+	private static Scanner entrada = new Scanner(System.in);
+	private static List<Contato> contatos=new ArrayList<>();
+	public static void main (String[] args) {
+		boolean sair = false;
+		while (!sair) = false{
+			
+		}
+	}
+}
